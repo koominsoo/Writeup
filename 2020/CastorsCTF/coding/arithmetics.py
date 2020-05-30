a1d67a3bd103d1d8ff6e64d699d6dcd00780eb5c
snwo <seonu64@gmail.com>
2020-05-30 13:02:01 +0900

    Update arithmetics.py

--- 2020/CastorsCTF/coding/arithmetics.py
+++ 2020/CastorsCTF/coding/arithmetics.py
@@ -4,7 +4,7 @@ r=remote("chals20.cybercastors.com",14429)
 r.recv()
 r.sendline("\n")
 d={'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-d2={'multiplied-by':'*','plus':'+','divided-by':'/','minus':'-'}
+d2={'multiplied-by':'*','plus':'+','divided-by':'//','minus':'-'}
 c=0
 def calc(a,op,b):
     global d
@@ -26,10 +26,11 @@ def calc(a,op,b):
         return int(a)*int(b)
 
 
-for i in range(55):
+for i in range(100):
     s=r.recv()
     s=s.split()
+    print(s)
     r.sendline(str(calc(s[2],s[3],s[4])))
-    print r.recvline()+str(c)
+    print r.recvline(timeout=1)+str(i)
 
 r.interactive()
