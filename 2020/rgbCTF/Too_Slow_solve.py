7f84e44f61719e05fbcef1186fefb9c122f14af0
snwo <seonu64@gmail.com>
2020-07-12 11:54:59 +0900

    rgb

---
+++ 2020/rgbCTF/Too_Slow_solve.py
@@ -0,0 +1,15 @@
+import binascii
+
+p='42642a41473e425717696a7c49334216426e2e4d79242e48796e714112297e12426e6f53'
+t=''
+for i in range(0,len(p),8):
+    t=p[i:i+8]+t
+flag=''
+for i in range(0,len(t),8):
+    a=hex(int(t[i:i+8],16)^0x265d1d23)[2:]
+    temp=''
+    for i in range(0,len(a),2):
+        temp=a[i:i+2]+temp
+    flag+=temp
+
+print('rgbCTF{'+binascii.unhexlify(flag)+'}')
